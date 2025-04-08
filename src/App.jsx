952547fed596b886a3818003fce0dbb473fd82b0
SamuelOsilva <159027401+SamuelOsilva@users.noreply.github.com>
2025-04-08 13:14:12 +0000

    08/04/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import Botao from "./componentes/Botao"
 import Caixa from "./componentes/Caixa"
 import Etiqueta from "./componentes/Etiqueta"
-
+ 
 const App = () => {
-
+ 
   return (
     <>
       Aula de Componentes
-
-
-      <Etiqueta  texto ="Texto enviado"/>
-      <Etiqueta  texto= "Outro texto"/>
-      <Etiqueta   texto= "mais um texto"/>
+ 
+      <Etiqueta texto="Texto enviado" />
+      <Etiqueta />
+      <Etiqueta />
+      <Caixa />
+      <Caixa />
       <Caixa />
       <Botao />
     </>
   )
 }
-
+ 
 export default App
